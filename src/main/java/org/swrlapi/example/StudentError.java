8adf5fb1808e4700f1bff19d0d4924f81d0d01fe
Nikita Penskoy <nik95penik@yandex.ru>
2020-06-02 12:09:15 +0400

    Move StudentError to src package

---
+++ src/main/java/org/swrlapi/example/StudentError.java
@@ -0,0 +1,60 @@
+package org.swrlapi.example;
+
+import java.util.Objects;
+
+enum StudentErrorType {
+    HIGH_PRIORITY_TO_LEFT,
+    HIGH_PRIORITY_TO_RIGHT,
+    LEFT_ASSOC_TO_LEFT,
+    RIGHT_ASSOC_TO_RIGHT,
+    IN_COMPLEX,
+    SAME_OPERATION,
+}
+
+class StudentError {
+    public StudentError(int errorPos, int reasonPos, StudentErrorType type) {
+        ErrorPos = errorPos;
+        ReasonPos = reasonPos;
+        Type = type;
+    }
+
+    public int getErrorPos() {
+        return ErrorPos;
+    }
+
+    public int getReasonPos() {
+        return ReasonPos;
+    }
+
+    public StudentErrorType getType() {
+        return Type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentError that = (StudentError) o;
+        return getErrorPos() == that.getErrorPos() &&
+                getReasonPos() == that.getReasonPos() &&
+                getType() == that.getType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getErrorPos(), getReasonPos(), getType());
+    }
+
+    @Override
+    public String toString() {
+        return "StudentError{" +
+                "ErrorPos=" + ErrorPos +
+                ", ReasonPos=" + ReasonPos +
+                ", Type=" + Type +
+                '}';
+    }
+
+    int ErrorPos;
+    int ReasonPos;
+    StudentErrorType Type;
+}
--- src/test/java/org/swrlapi/example/StudentErrorTest.java
+++ src/test/java/org/swrlapi/example/StudentErrorTest.java
@@ -1,72 +1,11 @@
 package org.swrlapi.example;
 
 import org.junit.jupiter.api.Test;
-import org.semanticweb.owlapi.model.OWLDataProperty;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.reasoner.Node;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.swrlapi.example.OntologyUtil.*;
-
-enum StudentErrorType {
-    HIGH_PRIORITY_TO_LEFT,
-    HIGH_PRIORITY_TO_RIGHT,
-    LEFT_ASSOC_TO_LEFT,
-    RIGHT_ASSOC_TO_RIGHT,
-    IN_COMPLEX,
-    SAME_OPERATION,
-}
-
-class StudentError {
-    public StudentError(int errorPos, int reasonPos, StudentErrorType type) {
-        ErrorPos = errorPos;
-        ReasonPos = reasonPos;
-        Type = type;
-    }
-
-    public int getErrorPos() {
-        return ErrorPos;
-    }
-
-    public int getReasonPos() {
-        return ReasonPos;
-    }
-
-    public StudentErrorType getType() {
-        return Type;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        StudentError that = (StudentError) o;
-        return getErrorPos() == that.getErrorPos() &&
-                getReasonPos() == that.getReasonPos() &&
-                getType() == that.getType();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getErrorPos(), getReasonPos(), getType());
-    }
-
-    @Override
-    public String toString() {
-        return "StudentError{" +
-                "ErrorPos=" + ErrorPos +
-                ", ReasonPos=" + ReasonPos +
-                ", Type=" + Type +
-                '}';
-    }
-
-    int ErrorPos;
-    int ReasonPos;
-    StudentErrorType Type;
-}
 
 class StudentErrorTest {
     Set<StudentError> GetErrors(List<String> texts, List<Optional<Integer>> studentPos) {
