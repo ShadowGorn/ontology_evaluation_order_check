d819a67d1706369324a23dc188e9403f2ca3868e
Nikita Penskoy <nik95penik@yandex.ru>
2021-02-19 21:06:51 +0300

    Return console

--- src/main/java/org/swrlapi/example/Main.java
+++ src/main/java/org/swrlapi/example/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        RestRequester.main(args);
+        ConsoleRequester.main(args);
     }
 }
 
