00b288b182bff4830aec58430013fbbd18062178
Nikita Penskoy <nik95penik@yandex.ru>
2021-02-19 21:03:27 +0300

    Make HTTP Server

---
+++ src/main/java/org/swrlapi/example/RestRequester.java
@@ -0,0 +1,40 @@
+package org.swrlapi.example;
+
+import com.sun.net.httpserver.Headers;
+import io.vavr.control.Try;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class RestRequester {
+    public static void main(String[] args) throws IOException {
+        JsonRequester requester = new JsonRequester();
+        int serverPort = 8000;
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+
+        server.createContext("/api/check", (exchange -> {
+            String response = requester.response(exchange.getRequestBody().toString());
+            Headers headers = new Headers();
+            headers.set("Content-Type", "application/json");
+            exchange.getResponseHeaders().putAll(headers);
+            exchange.sendResponseHeaders(200, response.length());;
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.flush();
+            os.close();
+            exchange.close();
+        }));
+
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+}
