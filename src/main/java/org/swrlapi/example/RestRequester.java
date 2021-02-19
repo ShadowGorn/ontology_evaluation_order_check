e03057c0fb1e7722ed56784051bf81b82b4188ea
Nikita Penskoy <nik95penik@yandex.ru>
2021-02-19 21:08:07 +0300

    Fix

--- src/main/java/org/swrlapi/example/RestRequester.java
+++ src/main/java/org/swrlapi/example/RestRequester.java
@@ -1,16 +1,9 @@
 package org.swrlapi.example;
 
 import com.sun.net.httpserver.Headers;
-import io.vavr.control.Try;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.HttpContext;
-import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
